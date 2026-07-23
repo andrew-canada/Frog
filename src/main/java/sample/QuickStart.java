ec05eac5d1ec458a45cf77557b27e6c1ff7a30a6
Eleanor Neal <eleanorneal2007@gmail.com>
2026-07-22 23:10:13 -0400

    Connected remote MongoDB database to sample program in project

---
+++ src/main/java/sample/QuickStart.java
@@ -0,0 +1,28 @@
+package sample;
+
+import static com.mongodb.client.model.Filters.eq;
+import org.bson.Document;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+// This is sample code for accessing the MongoDB database. To get the database working I created a free account,
+// then followed these instructions: https://www.mongodb.com/docs/drivers/java/sync/current/get-started
+
+public class QuickStart {
+    public static void main(String[] args) {
+        // Replace the placeholder with your MongoDB deployment's connection string
+        String uri = "mongodb+srv://eleanorneal_db_user:lNth5u9FuYk4NzPN@flushid.jpqnasb.mongodb.net/?appName=FlushID";
+        try (MongoClient mongoClient = MongoClients.create(uri)) {
+            MongoDatabase database = mongoClient.getDatabase("sample_mflix");
+            MongoCollection<Document> collection = database.getCollection("movies");
+            Document doc = collection.find(eq("title", "Alien")).first();
+            if (doc != null) {
+                System.out.println(doc.toJson());
+            } else {
+                System.out.println("No matching documents found.");
+            }
+        }
+    }
+}
