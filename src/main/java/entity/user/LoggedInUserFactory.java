78efdba24b4341b64eedc46f4eb1dda7c96301f4
Mihaly Dumitru Margereta Corneliu Leopold Blanca Karol Aeon Ignatius Rapahel Maria Niketas A Shilage <ia.wang@mail.utoronto.ca>
2026-07-22 22:12:16 -0400

    updates to entities

--- src/main/java/entity/user/LoggedInUserFactory.java
+++ src/main/java/entity/user/LoggedInUserFactory.java
@@ -1,4 +1,8 @@
 package entity.user;
 
-public class LoggedInUserFactory {
+public class LoggedInUserFactory implements UserFactory {
+    @Override
+    public User create(String name, String password) {
+        return new LoggedInUser(name, password);
+    }
 }
