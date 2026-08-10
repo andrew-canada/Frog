0be63e24b700ffcfddd4195d66184f9658e5a54f
mdmclbkairmnas <ia.wang@mail.utoronto.ca>
2026-08-10 17:01:49 -0400

    modify review sort

--- src/main/java/views/ReviewSortDropdownControl.java
+++ src/main/java/views/ReviewSortDropdownControl.java
@@ -4,7 +4,7 @@ import javax.swing.JComboBox;
 
 final class ReviewSortDropdownControl extends JComboBox<String> {
     ReviewSortDropdownControl() {
-        super(new String[] {"Highest Rated", "Lowest Rated", "Most Helpful", "Newest", "Voted by Me"});
+        super(new String[] {"Relevant", "Highest Rated", "Lowest Rated", "Most Helpful", "Newest", "Voted by Me"});
         setBackground(Theme.PAPER);
     }
 }
