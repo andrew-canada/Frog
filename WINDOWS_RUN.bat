6e8501528673cc65a2b47c878ff4018ae013dba6
Eleanor Neal <eleanor.neal@mail.utoronto.ca>
2026-08-10 04:43:47 -0400

    trying to add runnable files

---
+++ WINDOWS_RUN.bat
@@ -0,0 +1,5 @@
+set MONGODB_URI=mongodb+srv://eleanorneal_db_user:lNth5u9FuYk4NzPN@flushid.jpqnasb.mongodb.net/?appName=FlushID
+set MONGODB_DATABASE=FlushID
+set GRAPHHOPPER_API_KEY=1748f90f-16e3-40a7-840d-16a3428eb707
+set GEMINI_API_KEY=AQ.Ab8RN6JTaB9s9SAQxfYGrJB2DUfG9Xldcl2easfE5gAZ5VcauA
+java -jar .\out\artifacts\FlushID_jar\FlushID.jar
